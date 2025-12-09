4bf1569c72860811cf611befaf85cd06a0cc4caa
Kelvin Dieto <41842398+kmdieto@users.noreply.github.com>
2025-12-09 20:28:05 +0300

    Fix import case for Netlify build

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
-import ProductsPage from "./pages/productsPage";
+import ProductsPage from "./pages/ProductsPage";
 import { ThemeProvider } from "./context/ThemeContext";
 import { CartProvider } from "./context/cartContext";
 import "./App.css";
