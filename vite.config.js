b434103fac19f9fe2dec9d5289b25da8e9f1fee1
Kelvin Dieto <41842398+kmdieto@users.noreply.github.com>
2025-12-09 20:11:35 +0300

    Fix Vite root for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: 'app',
+  root: path.resolve(__dirname, 'app'),  // <-- folder where index.html lives
   plugins: [react()],
-})
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true
+  }
+});
